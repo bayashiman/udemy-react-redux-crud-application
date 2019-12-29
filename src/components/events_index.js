7c4e828f3a8db29942e084ea6e1971bbe6f4924e
kobayashi ryota <kobayashi@respawn.jp>
2019-12-29 19:02:30 +0900

    inplement events index

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,29 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { readEvents } from '../actions';
 class EventsIndex extends Component {
   componentDidMount() {
     this.props.readEvents()
   }
 
-  constructor(props) {
-    super(props)
-    this.state = { count: 0 }
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
   }
+  
   render() {
-    const props = this.props
-
     return (
-      <React.Fragment>
-        <div>counter: { props.value }</div>
-        <button onClick={props.increment}>+1</button>
-        <button onClick={props.decrement}>-1</button>
-      </React.Fragment>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {this.renderEvents()}
+        </tbody>
+      </table>
+      
     )
   }
 }
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({events: state.events})
 
 const mapDispatchToProps = ({ readEvents })
 
